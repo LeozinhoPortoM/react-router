496bfe57c6fcf1488bd168b7c5408f560ff0df41
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-10 22:08:17 -0300

    including dynamic router and style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,25 @@
 import "./style.css";
 
 import { Link } from "react-router-dom";
-import { UseEffect } from "react";
+import { useFetch } from "../../hooks/useFetch";
+const url = "http://localhost:3000/products";
 
 export default function Home() {
+  const { data: items, loading } = useFetch(url);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Produtos</h1>
+      <ul className="products">
+        {items &&
+          items.map((item) => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <p>{item.price}</p>
+              <Link to={`/products/${item.id}`}>Detalhes</Link>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
