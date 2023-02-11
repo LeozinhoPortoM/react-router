f815269d012a427188a1b4f2074dae41a5e792e8
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-10 22:04:22 -0300

    creating new product route

--- src/main/App.js
+++ src/main/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Product from "../pages/Product";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/products/:id" element={<Product />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
