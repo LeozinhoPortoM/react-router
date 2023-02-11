a1796ddf74754199998a74e670b690cddfb14b49
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-10 22:20:29 -0300

    insert details product

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,9 +1,23 @@
 import "./style.css";
 
+import { useParams } from "react-router-dom";
+import { Fragment } from "react";
+
+import { useFetch } from "../../hooks/useFetch";
+
 export default function Product() {
+  const { id } = useParams();
+  const url = "http://localhost:3000/products/" + id;
+  const { data: product } = useFetch(url);
   return (
-    <div>
-      <h1>Produto</h1>
-    </div>
+    <Fragment>
+      <p>ID do produto: {id}</p>
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>{product.price}</p>
+        </div>
+      )}
+    </Fragment>
   );
 }
