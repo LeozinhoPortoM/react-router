e9c8a174879afe122d7656bf560824aa4680be4a
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-10 21:39:30 -0300

    setting file and style Home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+import { Link } from "react-router-dom";
+import { UseEffect } from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
