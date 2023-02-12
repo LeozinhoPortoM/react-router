128a56486419000d0014d1502b92fd059bdc1fd6
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-11 21:57:40 -0300

    using NavLink

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./style.css";
 
 export default function Navbar() {
   return (
     <nav>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/about"}>About</Link>
+      {/* <Link to={"/"}>Home</Link>
+      <Link to={"/about"}>About</Link> */}
+      <NavLink to={"/"}>Home</NavLink>
+      <NavLink to={"/about"}>Sobre</NavLink>
     </nav>
   );
 }
