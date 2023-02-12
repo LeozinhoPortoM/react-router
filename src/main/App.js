654a144e85e9dd8de9d271fe4d2d813769f9b489
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-12 00:28:20 -0300

    adding search component and search route

--- src/main/App.js
+++ src/main/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 
+import SearchField from "../components/SearchField";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Product from "../pages/Product";
+import Search from "../pages/Search";
 
 export default function App() {
   return (
@@ -13,10 +16,12 @@ export default function App() {
       <h1>React router</h1>
       <BrowserRouter>
         <Navbar />
+        <SearchField />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products/:id" element={<Product />} />
+          <Route path="/search" element={<Search />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
